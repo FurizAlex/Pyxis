2dbe8ecf796b677189dd2e7be0a99221309a66a9
FurizAlex <55350194+FurizAlex@users.noreply.github.com>
2025-02-05 22:58:53 +0800

    Create example.pyx

---
+++ documentation/example.pyx
@@ -0,0 +1,20 @@
+extends #Normal cc::standard[@applepie]
+//this is an example of some code for pyxis, so the example we're doing is a recreation of strlen
+
+var str
+var length
+
+defi main():
+  while (str[length] !: nil):
+    length.add(1)!
+  return &length.source
+
+
+! = end of line marker
+!: = not
+:: = equals
+: = equal equal
+& = 
+;; = 
+// = comment
+@wrafs = static
